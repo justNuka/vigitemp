745fd3557f65c51df106ddc6ff8cfb7aa4df2c86
justNuka <elias59410@gmail.com>
2026-01-16 11:30:18 +0100

    Merge branch 'feat/update-serveur' of https://github.com/justNuka/vigitemp into feat/update-serveur

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -135,7 +135,7 @@ const config: Config = {
   		}
   	}
   },
-  darkMode: ["class", "class"],
+  darkMode: "class",
   plugins: [heroui(), require("tailwindcss-animate")],
 };
 export default config;
