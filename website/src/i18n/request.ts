6902b8ae8bc182e3f6bda14103675ed41b80bab3
justNuka <75932540+justNuka@users.noreply.github.com>
2026-08-21 12:24:09 +0200

    i18n: charger les libellés étalonnage enrichis

--- website/src/i18n/request.ts
+++ website/src/i18n/request.ts
@@ -2,6 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
 import {hasLocale} from 'next-intl';
 import {routing} from './routing';
 import {mergeMessages, supplementForLocale, type MessageCatalog} from '../messages/supplements';
+import {metrologyCalibrationSupplementForLocale} from '../messages/metrology-calibration-supplements';
  
 export default getRequestConfig(async ({requestLocale}) => {
   // Typically corresponds to the `[locale]` segment
@@ -13,9 +14,10 @@ export default getRequestConfig(async ({requestLocale}) => {
   const baseMessages = (
     await import(`../messages/${locale}.json`)
   ).default as MessageCatalog;
+  const messagesWithSupplements = mergeMessages(baseMessages, supplementForLocale(locale));
  
   return {
     locale,
-    messages: mergeMessages(baseMessages, supplementForLocale(locale))
+    messages: mergeMessages(messagesWithSupplements, metrologyCalibrationSupplementForLocale(locale))
   };
 });
