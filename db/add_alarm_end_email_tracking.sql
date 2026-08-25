9a370912eb719d841be38294ae4d39e99ecd9d2e
justNuka <elias59410@gmail.com>
2026-08-25 11:24:29 +0200

    update certaines entrées bdd (profil)

--- db/add_alarm_end_email_tracking.sql
+++
@@ -1,6 +0,0 @@
--- MySQL - base existante
-USE `vigi_main`;
-
-ALTER TABLE `t_alarme`
-  ADD COLUMN `Est_Mail_Fin_Envoye` tinyint(1) NOT NULL DEFAULT '0'
-  AFTER `Est_Mail_Envoye`;
--- db/vigisensys_seed_mssql.sql
+++ db/vigisensys_seed_mssql.sql
@@ -2463,11 +2463,6 @@ SET IDENTITY_INSERT dbo.t_profil ON;
 MERGE dbo.t_profil AS target
 USING (VALUES
   (1, N'Administrateurs', NULL, 0, 0),
-  (3, N'Consultation + Acquittement', NULL, 0, 0),
-  (7, N'VIGITEL', N'', 0, 0),
-  (8, N'ADMINistrateurs +', N'', 0, 0),
-  (9, N'Consultation + Acquittement + Desactivation', N'', 0, 0),
-  (12, N'Test ajout', NULL, 0, 0)
 ) AS source (Id_Profil, Profil_Utilisateur, Commentaire, Est_MC2, Est_Archive)
 ON target.Id_Profil = source.Id_Profil
 WHEN MATCHED THEN UPDATE SET
@@ -2559,8 +2554,8 @@ GO
 DECLARE @AdminProfilId INT = (SELECT TOP 1 Id_Profil FROM dbo.t_profil WHERE Profil_Utilisateur = N'Administrateurs');
 INSERT INTO dbo.t_liaison_profil_autorisation (Id_Profil, Id_Autorisation) SELECT @AdminProfilId, a.Id_Autorisation FROM dbo.t_autorisation a WHERE @AdminProfilId IS NOT NULL AND NOT EXISTS (SELECT 1 FROM dbo.t_liaison_profil_autorisation l WHERE l.Id_Profil = @AdminProfilId AND l.Id_Autorisation = a.Id_Autorisation);
 GO
-IF NOT EXISTS (SELECT 1 FROM dbo.t_utilisateur WHERE Login = N'admin') INSERT INTO dbo.t_utilisateur (Login, Mot_De_Passe, Est_Archive, Profil_Utilisateur, Est_Mot_De_Passe_Temporaire, Date_Creation, Date_Derniere_Modification_MDP) VALUES (N'admin', N'$2b$10$p794ptDulNuN5Md2j3Y6Ge2wEYRjaG3Er8CexJ8RkrD4er1A2AhXS', 0, N'Administrateurs', 1, CAST(GETDATE() AS DATE), GETDATE());
-ELSE UPDATE dbo.t_utilisateur SET Mot_De_Passe = N'$2b$10$p794ptDulNuN5Md2j3Y6Ge2wEYRjaG3Er8CexJ8RkrD4er1A2AhXS', Est_Mot_De_Passe_Temporaire = 1, Profil_Utilisateur = COALESCE(Profil_Utilisateur, N'Administrateurs'), Est_Archive = 0 WHERE Login = N'admin' AND (Mot_De_Passe IS NULL OR Est_Mot_De_Passe_Temporaire = 1);
+IF NOT EXISTS (SELECT 1 FROM dbo.t_utilisateur WHERE Login = N'admin') INSERT INTO dbo.t_utilisateur (Login, Mot_De_Passe, Est_Archive, Profil_Utilisateur, Est_Mot_De_Passe_Temporaire, Date_Creation, Date_Derniere_Modification_MDP) VALUES (N'admin', N'$2b$10$PkfaBJ2yztneolPwfIcaI.QX5ppoeMa5e1rDA2hspODHvQSYFSn6W', 0, N'Administrateurs', 1, CAST(GETDATE() AS DATE), GETDATE());
+ELSE UPDATE dbo.t_utilisateur SET Mot_De_Passe = N'$2b$10$PkfaBJ2yztneolPwfIcaI.QX5ppoeMa5e1rDA2hspODHvQSYFSn6W', Est_Mot_De_Passe_Temporaire = 1, Profil_Utilisateur = COALESCE(Profil_Utilisateur, N'Administrateurs'), Est_Archive = 0 WHERE Login = N'admin' AND (Mot_De_Passe IS NULL OR Est_Mot_De_Passe_Temporaire = 1);
 GO
 
 USE [vigi_main];
