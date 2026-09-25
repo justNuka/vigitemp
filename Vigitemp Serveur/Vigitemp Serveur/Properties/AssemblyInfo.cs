a87938c538ed96ba61d7826206cdb087d57e9c4e
justNuka <75932540+justNuka@users.noreply.github.com>
2026-09-25 10:49:44 +0200

    chore(server): bump product version to 1.1.2

--- Vigitemp Serveur/Vigitemp Serveur/Properties/AssemblyInfo.cs
+++ Vigitemp Serveur/Vigitemp Serveur/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.90.1.0")]
 [assembly: AssemblyFileVersion("0.90.1.0")]
 // Version produit SemVer du serveur.
-[assembly: AssemblyInformationalVersion("1.1.1")]
+[assembly: AssemblyInformationalVersion("1.1.2")]
